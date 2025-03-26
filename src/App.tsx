a9990bc13a18beaa0a3ca04bea51e0578fd1fbed
Lagradante <jaynel.lagradante@accenture.com>
2025-03-26 11:31:53 +0800

    Added constant for routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,25 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import ViewTaskPage from './pages/ViewTaskPage';
 import Authentication from './hooks/Authentication';
+import { ROUTES } from './constants/Routes';
 
 function App() {
     return (
         <LocalizationProvider dateAdapter={AdapterMoment}>
             <Router>
                 <Routes>
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+                    <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
 
                     <Route
                         path="/*"
                         element={
                             <Authentication>
                                 <Routes>
-                                    <Route path="/" element={<TaskListPage />} />
-                                    <Route path="/create-task" element={<TaskPage />} />
-                                    <Route path="/edit-task/:id" element={<TaskPage />} />
-                                    <Route path="/view-task/:id" element={<ViewTaskPage />} />
+                                    <Route path={ROUTES.DASHBOARD} element={<TaskListPage />} />
+                                    <Route path={ROUTES.CREATE_TASK} element={<TaskPage />} />
+                                    <Route path={ROUTES.EDIT_TASK} element={<TaskPage />} />
+                                    <Route path={ROUTES.VIEW_TASK} element={<ViewTaskPage />} />
                                 </Routes>
                             </Authentication>
                         }
---
+++ src/constants/Routes.tsx
@@ -0,0 +1,8 @@
+export const ROUTES = {
+    DASHBOARD: '/',
+    LOGIN: '/login',
+    REGISTER: '/register',
+    CREATE_TASK: '/create-task',
+    EDIT_TASK: '/edit-task/:id',
+    VIEW_TASK: '/view-task/:id',
+};
